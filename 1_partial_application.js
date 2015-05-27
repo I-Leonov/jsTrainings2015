73b418517fafb2e105c329c4727db0cd4d9fa29e
Igor.Leonov <ileolfw@gmail.com>
2015-05-27 14:22:08 +0300

    partial

---
+++ 1_partial_application.js
@@ -0,0 +1,27 @@
+/**
+ * Created by Igor.Leonov on 5/26/2015.
+ */
+
+function sum() {
+    var args = Array.prototype.slice.call(arguments);
+    var sum = 0;
+    for (var i=0; i<args.length; i++) {
+        sum += +args[i];
+    }
+    return sum;
+}
+
+function partial() {
+    var fn = arguments[0];
+    var args = Array.prototype.slice.call(arguments,1);
+
+    return function() {
+        var myargs = Array.prototype.slice.call(arguments);
+        return fn.apply(null, args.concat(myargs));
+    }
+
+}
+
+
+var func = partial(sum,1,2);
+console.log(func(2,3));
