4969edc4654ba54f1d609eb351386e924bbb4318
Igor.Leonov <ileolfw@gmail.com>
2015-05-27 14:22:59 +0300

    folding

---
+++ 3_linear_folding.js
@@ -0,0 +1,22 @@
+/**
+ * Created by Igor.Leonov on 5/26/2015.
+ */
+var sum = function (a, b, index, arr) {
+    return a + b;
+};
+var linearFold = function (arr, callback, initialValue) {
+
+    if (initialValue == null) {
+        initialValue = 0;
+    }
+    for (var i = 0; i<arr.length; i++) {
+        initialValue = callback(initialValue, arr[i], i, arr);
+    }
+    return initialValue;
+};
+var arr = [1, 2, 3];
+console.log(linearFold(arr, sum));
+console.log(linearFold(arr, sum, 2));
+
+//ES5 alternative
+console.log(arr.reduce(sum));
