e745cdaeb5b0bef3f2d21db6c629b3f739276904
Igor.Leonov <ileolfw@gmail.com>
2015-06-11 17:08:26 +0300

    changes for error handling

--- logger/js/logger/logger.js
+++ logger/js/logger/logger.js
@@ -1,28 +1,63 @@
+define(['./alertLogger', './consoleLogger'],
+    function (alertLogger, consoleLogger) {
 
-define(['./alertLogger', './consoleLogger', './windowLogger'],
-    function (alertLogger, consoleLogger, windowLogger) {
-
-        var innerLog = function(logger, message) {
+        var innerLog = function (logger, message) {
             logger.log(message);
         };
 
-        var Abstract = function(logger) {
+        var Abstract = function (logger) {
             var logFunction = innerLog.bind(this, logger);
-            return {
-                log : logFunction
-            }
-        } ;
-        var Console = new Abstract(consoleLogger);
+            var enabled = false;
+            var oldErrorHandler = window ? window.onerror : undefined;
 
+            var globalErrorHandler = function (enable) {
+
+                if (enable === true) {
+                    if (enabled == true) {
+                        console.log("global error handling is already enabled for this logger")
+                    } else {
+                        enabled = true;
+                        window.onerror = function(errorMsg, url, lineNumber) {
+                            if (enabled) {
+
+                                logFunction(errorMsg);
+                            }
+                            oldErrorHandler(errorMsg, url, lineNumber);
+
+                        }
+                    }
+                } else {
+                    enabled = false;
+                }
+
+            };
+
+            return {
+                log: logFunction,
+                globalErrorHandler: globalErrorHandler
+            }
+        };
+
+        var addLogger = function (name, logger) {
+            if (name == null) {
+                console.error('name should not be null');
+            } else if (name in this) {
+                console.error('logger with such name is already registered');
+            } else {
+                this[name] = new Abstract(logger);
+            }
+        };
+
+        var Console = new Abstract(consoleLogger);
         var Alert = new Abstract(alertLogger);
-        var Window = new Abstract(windowLogger);
 
 
 
         return {
             Console: Console,
             Alert: Alert,
-            Window: Window
+            addLogger: addLogger
+
         }
 
     });
--- logger/js/main.js
+++ logger/js/main.js
@@ -1,8 +1,11 @@
-define(['logger/logger'],
-    function (logger) {
+define(['logger/logger','logger/windowLogger'],
+    function (logger,windowLogger) {
        logger.Console.log("test");
+        logger.Alert.globalErrorHandler(true);
+        logger.Asdfg.globalErrorHandler(true);
+
        //logger.Alert.log("test");
-       logger.Window.log("test");
-       logger.Window.log("test2");
+       //logger.Window.log("test");
+       //logger.Window.log("test2");
 
     });
